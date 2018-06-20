9c65bcf94bfdf885c5d16c306c4156805e30916f
Varun Amachi <varunamachi@gmail.com>
2018-06-20 21:18:45 +0530

    Connect example

---
+++ src/Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Constants {
+    static const char *SSID;
+    static const char *PASSWORD;
+    static const char *SECRET;
+    static const char *HOST;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,55 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
+#include "Constants.h" 
+
 void setup() {
+    // Serial.begin(115200); 
+    // Serial.println();
+
+    // WiFi.mode(WIFI_STA);
+    // WiFi.disconnect();
+    // delay(100);
+
     Serial.begin(115200);
     Serial.println();
-
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
+    
+    WiFi.begin(Constants::SSID, Constants::PASSWORD);
+    
+    Serial.print("Connecting...");
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    Serial.println("Scanning...");
-    auto numFound = WiFi.scanNetworks();
-    Serial.printf("Number of nw found: %d\n", numFound);
-    for (int i = 0; i < numFound; ++ i ) {
-        Serial.println(WiFi.SSID(i));
+    WiFiClient client;
+    Serial.printf("\n[Connecting to %s ... ", Constants::HOST);
+    if (client.connect(Constants::HOST, 80))
+    {
+        Serial.println("connected]");   
+        Serial.println("[Sending a request]");
+        client.print(String("GET /") + " HTTP/1.1\r\n" +
+                     "Host: " + Constants::HOST + "\r\n" +
+                     "Connection: close\r\n" +
+                     "\r\n" );  
+        Serial.println("[Response:]");
+        while (client.connected()) {
+            if (client.available()) {
+              String line = client.readStringUntil('\n');
+              Serial.println(line);
+            }
+        }
+        client.stop();
+        Serial.println("\n[Disconnected]");
     }
-    Serial.println();
-    delay(3000);
+    else {
+        Serial.println("connection failed!]");
+        client.stop();
+    }
+    delay(5000);
 }
