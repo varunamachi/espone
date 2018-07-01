77b52419fda16c0f2569116f5d5e617290136c67
Varun Amachi <varunamachi@gmail.com>
2018-07-01 19:45:08 +0530

    Made SecretImpl.h a secret

--- makesecret.sh
+++ makesecret.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ] || [ "$1" = "set" ] ; then
-    echo "Asking git to IGNORE changes to src/Secrets.cpp"
-    git update-index --assume-unchanged src/Secrets.cpp
+    echo "Asking git to IGNORE changes to src/SecretsImpl.h"
+    # git update-index --assume-unchanged src/Secrets.cpp
+    git update-index --skip-worktree src/SecretsImpl.h
 elif [ "$1" = "unset" ] ; then
-    echo "Asking git to CONSIDER changes to src/Secrets.cpp"
-    git update-index --no-assume-unchanged src/Secrets.cpp
+    echo "Asking git to CONSIDER changes to src/SecretsImpl.cpp"
+    # git update-index --no-assume-unchanged src/Secrets.cpp
+    git update-index --no-skip-worktree src/SecretsImpl.cpp
 fi
